7912627d5a496496d69b831f2d3f111ec7605d18
Punchline <re70977za@outlook.com>
2022-09-11 15:32:17 +0430

    added some other functions and objects

--- sandbox.js
+++ sandbox.js
@@ -13,3 +13,35 @@ const bill = (products, tax) => {
 }
 
 console.log(bill([10,20,100],50))
+
+// " This " keyword and Store Objects in Arrays
+const Blogs = [
+    {titles: 'My dead wife', likes: 30},
+    {titles: "I'm a vangaurd ", likes: 20}
+]
+
+let user = {
+
+    name: 'Zavala',
+    age: 30,
+    email: 'zavala@gmail.com',
+    location: 'The last city',
+    blogs: Blogs,
+
+    Login() {
+        console.log("You are logged in")
+    },
+
+    Logut() {
+        console.log("You are logged out")
+    },
+
+    LogBlogs() {
+        console.log("You have published these : ")
+        this.blogs.forEach(blog => {console.log(blog.titles, blog.likes)});
+    }
+
+};
+
+user.LogBlogs()
+
